b4c9e80205a8582904dfe7f924ac8f8177f535a7
Alexandra <a.r.chumachenko@gmail.com>
2021-11-13 23:31:46 +0200

    Add data project

---
+++ DataShopEntityFramework/Repositories/IRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataShopEntityFramework.Repositories
+{
+    interface IRepository<TEntity> where TEntity: class
+    {
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task UpdateAsync(TEntity entity);
+
+        Task DeleteAsync(TEntity entity);
+
+      
+        Task SaveChangesAsync();
+
+        Task<TEntity> GetByIdAsync(int id);
+
+        Task<TEntity> GetByIdAsync<TId>(TId id);
+
+        Task<List<TEntity>> ListAsync();
+        /*
+            Task<List<TEntity>> ListAsync(ISpecification<TEntity> specification);
+            Task<int> CountAsync(ISpecification<TEntity> specification);
+            Task DeleteRangeAsync(IEnumerable<TEntity> entities);
+        */
+
+    }
+}
