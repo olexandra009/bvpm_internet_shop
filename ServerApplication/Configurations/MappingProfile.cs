2136094c5ea5ccfaabbc9442c6c571041e544ec0
Alexandra <a.r.chumachenko@gmail.com>
2021-11-28 16:35:43 +0200

    Add mapping profile

--- ServerApplication/Configurations/MappingProfile.cs
+++ ServerApplication/Configurations/MappingProfile.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
+using DataShopEntityFramework.Entities;
+using ServerApplication.DTO;
+using ServerApplication.Models;
 
 namespace ServerApplication.Configurations
 {
@@ -10,6 +13,31 @@ namespace ServerApplication.Configurations
     {
         public MappingProfile()
         {
+            CreateMap<Category, CategoryModel>();
+            CreateMap<CategoryModel, Category>();
+            CreateMap<CategoryModel, CategoryDto>();
+            CreateMap<CategoryDto, CategoryModel>();
+
+            CreateMap<OrderDetail, OrderDetailsModel>();
+            CreateMap<OrderDetailsModel, OrderDetail>();
+            CreateMap<OrderDetailsModel, OrderDetailDto>();
+            CreateMap<OrderDetailDto, OrderDetailsModel>();
+
+            CreateMap<Order, OrderModel>();
+            CreateMap<OrderModel, Order>();
+            CreateMap<OrderModel, OrderDto>();
+            CreateMap<OrderDto, OrderModel>();
+
+            CreateMap<Product, ProductModel>();
+            CreateMap<ProductModel, Product>();
+            CreateMap<ProductModel, ProductDto>();
+            CreateMap<ProductDto, ProductModel>();
+
+            CreateMap<Review, ReviewModel>();
+            CreateMap<ReviewModel, Review>();
+            CreateMap<ReviewModel, ReviewDto>();
+            CreateMap<ReviewDto, ReviewModel>();
+
         }
     }
 }
